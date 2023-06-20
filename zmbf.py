596d0b8e7b0e7ee795f49f06386cd7f6071c0562
Jun pakaya <110225865+ONE-Idz@users.noreply.github.com>
2023-06-20 18:53:42 +0800

    Add files via upload

---
+++ zmbf.py
@@ -0,0 +1,47 @@
+import requests,re,rich,sys,os,json,time
+from rich.panel import Panel as panel
+from rich.panel import Panel as nel
+
+#-----------------------[ MASUK & INSTALL MODUL ]--------------------#
+if __name__=='__main__':
+	try:os.system('clear')
+	except:pass
+	
+	try:os.system('touch .prox.txt')
+	except:pass
+	
+	try:
+		import npyscreen
+	except ImportError:
+		os.system('clear')
+		prints(Panel(f"""{H2}• {P2}Modul npyscreen Proses {H2}•""",width=55,padding=(0,15),style=f"{color_panel}"))
+		os.system('pip install npyscreen && git pull')
+		
+	try:
+		import rich
+	except ImportError:
+		prints(Panel(f"""{H2}• {P2}Modul rich Proses {H2}•""",width=55,padding=(0,15),style=f"{color_panel}"))
+		os.system('pip install rich')
+	try:os.mkdir('/sdcard/JUNZ-DUMP')
+	except:pass
+	
+	try:
+		import stdiomask
+	except ImportError:
+		prints(Panel(f"""{H2}• {P2}Modul stdiomask Proses {H2}•""",width=55,padding=(0,15),style=f"{color_panel}"))
+		os.system('pip install stdiomask')
+	try:os.mkdir('/sdcard/Film Bokep18+')
+	except:pass
+		
+	try:
+		import requests
+	except ImportError:
+		prints(Panel(f"""{H2}• {P2}Modul requests Proses {H2}•""",width=55,padding=(0,15),style=f"{color_panel}"))
+		os.system('pip install requests && pip install mechanize ')
+	try:os.system('clear')
+	except:pass
+	try:os.system('cd && python colm3x.py')
+	except:pass
+
+	
+	
